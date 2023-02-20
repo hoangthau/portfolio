1114ef54e363008e85bd5e168ad4e14d545e20f3
Thau Nguyen <nghoangthau@gmail.com>
2023-02-20 10:27:40 +0700

    Update from main 1

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
         </SectionTitle>
         <SectionText>
           Hello everyone, This is Thau. I've been working as Frontend developer for about 8 years.
-          I'm passionate about JavaScript, TypeScript and ReactJS.
+          I'm passionate about JavaScript, TypeScript and ReactJS. Update from main.
         </SectionText>
         <Button
           onClick={(e) => {
