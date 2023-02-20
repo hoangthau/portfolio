3bb7788ac49e35bc69ed34c4bdfd2b96a7ddad89
Thau Nguyen <nghoangthau@gmail.com>
2023-02-20 10:25:34 +0700

    Change hero 2

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
         </SectionTitle>
         <SectionText>
           Hello everyone, This is Thau. I've been working as Frontend developer for about 8 years.
-          I'm passionate about JavaScript, TypeScript and ReactJS. Change hero 1.
+          I'm passionate about JavaScript, TypeScript and ReactJS. Change hero 1. Change hero 2
         </SectionText>
         <Button
           onClick={(e) => {
